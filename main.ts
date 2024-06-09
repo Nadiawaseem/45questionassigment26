6ac27db6e45507878bfa2dd05f01dd2f1e88098b
nadiawaseem <161966596+Nadiawaseem@users.noreply.github.com>
2024-06-08 17:07:34 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,17 @@
+  let alien_color = "green";
+
+  if (alien_color === "green") {
+    console.log("the player just earne 5 points.");
+
+  } else{
+    console.log("the players just earned 10points.");
+  }
+  let alien__color = "green";
+
+  if (alien_color === "yellow") {
+    console.log("the player just earne 5 points.");
+
+  } else{
+    console.log("the players just earned 10points.");
+  }
+  
